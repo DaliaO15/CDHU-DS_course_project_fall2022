703f11153cafc165a62fe94cf5343e45dc5af304
erno3194 <erik.nor@hotmail.se>
2022-11-08 16:54:05 +0100

    cropping on content of a folder

---
+++ basic_fd_cropping_CNN.py
@@ -0,0 +1,97 @@
+import utiles
+import argparse
+import imutils
+import time
+import dlib
+import cv2
+import os
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-sf", "--start_folder", type=str, required=True,
+	help="path to folder with input images")
+ap.add_argument("-ef", "--end_folder", type=str, required=True,
+	help="path to output (croped) images")
+ap.add_argument("-m", "--model", type=str,
+	default="mmod_human_face_detector.dat",
+	help="path to dlib's CNN face detector model")
+ap.add_argument("-u", "--upsample", type=int, default=1,
+	help="# of times to upsample")
+args = vars(ap.parse_args())
+
+# Loading the Face detector with the pre-trained model
+detector = dlib.cnn_face_detection_model_v1(args["model"])
+
+# assign directory
+directory = args["start_folder"]
+
+start_run = time.time()
+print('Iterating over files in directory ' + args["start_folder"])
+
+nr_fcs = 0
+tot_img = 0
+
+# iterate over files in
+# that directory
+for i, filename in enumerate(os.listdir(directory)):
+    f = os.path.join(directory, filename)
+    
+    # checking if it is a file
+    if os.path.isfile(f):
+        # Child directroy
+        subfolder = os.path.splitext(os.path.basename(f))[0]
+
+        # Parent Directory path
+        # TODO: Change name of end_folder
+        end_folder = args['end_folder']
+
+        # Path
+        path = os.path.join(end_folder, subfolder)
+        
+        # Create a subfolder for the current image in the end_folder
+        #os.mkdir(path)
+        os.chdir(end_folder)
+        os.mkdir(subfolder)
+        os.chdir('..')
+        
+        print(f)
+        img = cv2.imread(f)
+        img = imutils.resize(img, width=600)
+        rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        # Detection
+        start = time.time()
+        print('Face detection with CNN started on image ' + filename)
+        rects = detector(rgb, args["upsample"]) # this is a list
+        end = time.time()
+        print(f'Run time on image {end-start}')
+
+        # Fix the boxes
+        boxes = [utiles.trim_n_convert_v2(image=img, epsilon = 5, rectangle=r.rect) for r in rects]
+
+        # To store images in the desired folder
+        os.chdir(path)
+        
+        box_count = 0
+        
+        # loop over the bounding boxes
+        for (x, y, w, h) in boxes:
+            # draw the bounding box on our image
+            fcs = img[y:y+h, x:x+w]
+            fcs = imutils.resize(fcs, width=100)
+            cv2.imwrite('face'+str(box_count)+'.jpg', fcs)
+            # cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            box_count += 1
+            
+        nr_fcs += box_count
+        tot_img = i
+
+        # Going back to root directory
+        #print(os.getcwd())
+        os.chdir('../../') # TO BE CHANGED ACCORDINGLY
+        
+print('Processed ' + str(tot_img+1) + ' images')
+print('Detected ' + str(nr_fcs) + ' faces')
+
+end_run = time.time()
+print(f'Total runtime {end_run-start_run}')
